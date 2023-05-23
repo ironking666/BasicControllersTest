22d8f4344c679dc4a0a69d43810699e130f24552
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2023-05-23 18:38:25 +0200

    Hover Page Test and Dropdown Page Tests

---
+++ cypress/e2e/dropdownPageTest.cy.js
@@ -0,0 +1,15 @@
+import HomePage from "../page-objects/homePage";
+import DropdownListPage from "../page-objects/dropdownListPage";
+
+describe("dropdown page test", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("test dropdown list", () => {
+    const homePage = new HomePage();
+    homePage.clickDropdownListTab();
+
+    const dropdownPage = new DropdownListPage();
+    dropdownPage.chooseDropdownListFirstOption();
+  });
+});
